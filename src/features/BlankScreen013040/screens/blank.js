ef3c316bc5e0b2a57d4d799149917be05d8e8003
Crowdbotics <team@crowdbotics.com>
2020-10-23 05:08:35 +0000

    Update components

--- src/features/BlankScreen013040/screens/blank.js
+++ src/features/BlankScreen013040/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,52 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Text>Sample text content</Text>
+      <Button
+        title="Press me!"
+        color="#33ff4b"
+        style={styles.Button_3}
+        onPress={() => alert("Pressed!")}
+      />
+      <Slider
+        value={50}
+        minimumValue={0}
+        maximumValue={100}
+        maximumTrackTintColor="#48ae24"
+        minimumTrackTintColor="#ff3347"
+        thumbTintColor="#1b221e"
+        style={styles.Slider_4}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  View_1: {},
+  Text_2: {},
+  Button_3: { color: "#e42121" },
+  Slider_4: { width: "100%" }
+})
